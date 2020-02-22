072375ad925581f4e8645ebf26261c8551da8092
Igor Krupin <clearwaterstream@gmail.com>
2020-02-21 21:55:31 -0500

    a

---
+++ src/AWSLambda.AspNetCoreInterop.LocalRouter/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+
+namespace AWSLambda.AspNetCoreInterop.LocalRouter
+{
+    class Program
+    {
+        static ILogger<Program> logger;
+        static readonly CancellationTokenSource appCtSource = new CancellationTokenSource();
+
+        static void Main(string[] args)
+        {
+            ConsoleUtil.WriteProgramTitle("AWS Lambda ASP .NET Core Interop");
+
+            var builder = CreateHostBuilder(args);
+
+            var host = builder.Build();
+
+            logger = host.Services.GetService<ILogger<Program>>();
+
+            logger.LogInformation("Router bootstrapped");
+
+            host.Run();
+
+            Console.Read();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/AWSLambda.AspNetCoreInterop.LocalRouter/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AWSLambda.AspNetCoreInterop.LocalRouter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(opt => opt.AddConsole(c =>
+            {
+                c.TimestampFormat = "[HH:mm:ss.ffff] ";
+            }));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("AWS Lambda ASP .NET Core Interop");
+                });
+            });
+        }
+    }
+}
